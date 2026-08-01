fc1b5ab977c6499e30f00ad21b8c5bf9d637cf56
TranMK <tran2ki@mail.uc.edu>
2026-07-31 21:51:15 -0400

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DatabasesProject-TranMK
+A final project made for Database Design and Development (CS4092) - Summer 2026.  
+Instructor: Dr. Hrishikesh Bhide
+Student Name: Khoi Tran
+
+# Description
+This project is a hands-on experience designing and implementing a relational database system, built using Python and SQL.
--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 import mysql.connector
- 
+import sqlite3
 dataBase = mysql.connector.connect(
   host ="localhost",                # Localhost for local connection
-  user ="user",
-  passwd ="password"
+  user ="khoitran",
+  passwd ="khoitranpassword"
 )
+# preparing a cursor object
+cursorObject = dataBase.cursor()
 
+# creating database
+cursorObject.execute("CREATE DATABASE gfg")
 print(dataBase)
 dataBase.close()
